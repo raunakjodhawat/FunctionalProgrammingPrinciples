68eb74e11d72fab94b7712dd864ec114f0498c81
Raunak Jodhawat <raunakjodhawat@gmail.com>
2020-10-30 13:04:04 -0400

    p1s3 add whileLoop creation

--- src/main/scala/p1s3/FunctionalDataStructure.scala
+++ src/main/scala/p1s3/FunctionalDataStructure.scala
@@ -2,8 +2,10 @@ package p1s3
 
 
 sealed trait List[+A]
+
 case object Nil extends List[Nothing]
-case class Cons[+A] (head: A, tail: List[A]) extends List[A]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 
 object List {
@@ -19,7 +21,7 @@ object List {
   }
 
   def apply[A](as: A*): List[A] =
-    if(as.isEmpty) Nil
+    if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
   // Exercise 3.2
@@ -35,18 +37,68 @@ object List {
   }
 
   def addAtFront[A](vals: List[A], elem: A) = Cons(elem, vals)
+
+  // Exercise 3.4
+  def drop[A](l:List[A], n: Int): List[A] = {
+    def loop(i: Int, l1: List[A]): List[A] = {
+      if(i==n) l1
+      else loop(i+1, removeFirst(l1))
+    }
+    loop(0, l)
+  }
+
+  // Exercise 3.5
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    def individualChecker(l1: List[A]): List[A] = {
+      l1 match {
+        case Nil => l1
+        case Cons(x, xs) =>
+          if(f(x)) xs
+          else Cons(x, xs)
+      }
+    }
+
+    def loop(l1: List[A]): List[A] = {
+      l1 match {
+        case Nil => l1
+        case Cons(x, xs) => individualChecker(Cons(x, loop(xs)))
+      }
+    }
+    loop(l)
+  }
+
 }
 
 object FunctionalDataStructure {
 
   def main(args: Array[String]): Unit = {
-    val ex1: List[Double] = Nil
-    val ex2: List[Int] = Cons(100, Nil)
-    val ex3: List[Boolean] = Cons(true, Cons(false, Nil))
 
-    println(ex1)
-    println(ex2)
-    println(ex3)
+    val l1 = List(1, 2, 3, 4)
+    println(l1)
+    val l2 = List.removeFirst(l1)
+    println(l2)
+    println(l1)
 
+    val l3 = List.changeHead(l2, 30)
+
+    println(l3)
+    println(l2)
+    
+    val l4 = List.addAtFront(l3, 100)
+
+    println(l4)
+    println(l3)
+
+    println(List.drop(l3, 2))
+    println(List.drop(l3, 0))
+    println(List.drop(l3, 3))
+
+
+    val l5 = List(10, 12, 15, 20, 22, 32, 42, 52, 60)
+
+    println(l5)
+    val l6 = List.dropWhile(l5, (a: Int) => a%5 == 0)
+    println(l6)
+    println("all done")
   }
 }
